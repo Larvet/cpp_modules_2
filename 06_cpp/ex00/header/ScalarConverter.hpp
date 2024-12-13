117163efd6adc8ae9c0b7106401630245f92a011
Louis Charvet <locharve@1E7.42angouleme.fr>
2024-12-13 16:20:53 +0100

    06/

---
+++ 06_cpp/ex00/header/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 16:09:15 by locharve          #+#    #+#             */
+/*   Updated: 2024/12/13 16:18:33 by locharve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+# include <cmath>
+
+class	ScalarConverter {
+	public:
+		static convert(std::string str);
+};
+
+#endif

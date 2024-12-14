5cbd0ebfa524e2f1fd728c6c83a8767e5e6e34d9
Louis Charvet <locharve@2F4.42angouleme.fr>
2024-12-14 15:53:48 +0100

    ex00 suite

--- 06_cpp/ex00/src/ScalarConverter.cpp
+++ 06_cpp/ex00/src/ScalarConverter.cpp
@@ -6,8 +6,82 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 16:17:00 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/13 16:17:09 by locharve         ###   ########.fr       */
+/*   Updated: 2024/12/14 15:52:02 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& src) {
+	(void)src;
+}
+
+ScalarConverter&	ScalarConverter::operator=(const ScalarConverter& src) {
+	(void)src;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+static void	handleSpecial(std::string str) {
+	double	d = atof(str.c_str());
+	float	f = static_cast<float>(d);
+	std::cout << "char:\timpossible" << std::endl
+		<< "int:\timpossible" << std::endl
+		<< "float:\t";
+		if (str[0] == '+')
+			std::cout << '+';
+		std::cout << f << 'f' << std::endl
+		<< "double:\t";
+		if (str[0] == '+')
+			std::cout << '+';
+		std::cout << d << std::endl;
+}
+
+static bool	isConvertible(std::string str) {
+	std::string::iterator	it = str.begin();
+	std::string::iterator	end = str.end();
+	while (it != end && std::isdigit(*it))
+		it++;
+	if (it == end)
+		return (true);
+	if (*it != '.')
+		return (false);
+	it++;
+	if (!std::isdigit(*it))
+		return (false);
+	while (it != end && std::isdigit(*it))
+		it++;
+	return (it == end || *it == 'f');
+}
+
+void	ScalarConverter::convert(std::string str) {
+	if (str == "nan" || str == "nanf" || str == "-inf"
+		|| str == "-inff" || str == "+inf" || str == "+inff")
+		handleSpecial(str);
+	else if (!isConvertible(str))
+		std::cout << "impossible: " << str << std::endl;
+//		handleImpossible();
+	else {
+		double	d = atof(str.c_str());
+		float	f = static_cast<float>(d);
+		int		i = static_cast<int>(d);
+		if (i >= 33 && i <= 126) {
+			char	c = static_cast<char>(d);
+			std::cout << "char:\t'" << c << "'" << std::endl;
+		} else {
+			std::cout << "char:\t" << "Non displayable" << std::endl;
+		}
+		std::cout << "int:\t" << i << std::endl
+			<< "float:\t" << f;
+		if (f == static_cast<int>(i))
+			std::cout << ".0";
+		std::cout << 'f' << std::endl
+			<< "double:\t" << d;
+		if (d == static_cast<int>(i))
+			std::cout << ".0";
+		std::cout << std::endl;
+	}
+}
--- 06_cpp/ex00/src/main.cpp
+++ 06_cpp/ex00/src/main.cpp
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/13 16:09:15 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/14 15:38:15 by locharve         ###   ########.fr       */
+/*   Created: 2024/12/14 13:52:32 by locharve          #+#    #+#             */
+/*   Updated: 2024/12/14 14:07:17 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCALARCONVERTER_HPP
-# define SCALARCONVERTER_HPP
+#include "ScalarConverter.hpp"
 
-# include <iostream>
-# include <cstdlib>
-# include <cctype>
-
-class	ScalarConverter {
-	private:
-		ScalarConverter();
-	public:
-		ScalarConverter(const ScalarConverter& src);
-		ScalarConverter& operator=(const ScalarConverter& src);
-		~ScalarConverter();
-		
-		static void convert(std::string str);
-};
-
-#endif
+int	main(int argc, char** argv) {
+	if (argc == 2) {
+	//	ScalarConverter	s;
+	//	s.convert(argv[1]);
+		ScalarConverter::convert(argv[1]);
+	}
+	else
+		std::cerr << "This program needs one argument" << std::endl;
+} 

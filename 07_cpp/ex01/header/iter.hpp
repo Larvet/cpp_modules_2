74fb2b1abfed31eabbf8168630d6c9f157ab4c37
Louis Charvet <locharve@2C4.42angouleme.fr>
2025-01-02 11:40:30 +0100

    ex01

--- 07_cpp/ex01/header/iter.hpp
+++ 07_cpp/ex01/header/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:37:06 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/18 15:49:07 by locharve         ###   ########.fr       */
+/*   Updated: 2025/01/02 08:52:41 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,44 @@
 
 # include <iostream>
 
+class	ExampleClass {
+	private:
+		int	_n;
+		void*	_ptr;
+	public:
+		ExampleClass(int n, void* ptr): _n(n), _ptr(ptr) {}
+		ExampleClass() { _n = 42; _ptr = NULL; }
+		ExampleClass(const ExampleClass& src) { *this = src; }
+		ExampleClass&	operator=(const ExampleClass& src) { _n = src.getN(); _ptr = src.getPtr(); return (*this); }
+		ExampleClass&	operator=(const int& n) { _n = n; return (*this); }
+		~ExampleClass() {}
+
+		void	setN(int n) { _n = n; }
+		void	setPtr(void* ptr) { _ptr = ptr; }
+
+		int	getN() const { return (_n); }
+		void*	getPtr() const { return (_ptr); }
+
+	//	ExampleClass&	operator+=(const int& n) { _n += n; return (*this); }
+
+		bool	operator<(const ExampleClass& ex) const { return (_n < ex.getN()); }
+	//	bool	operator<(const ExampleClass& ex) const { return (_ptr < ex.getPtr()); }		
+
+		bool	operator>(const ExampleClass& ex) const { return (_n > ex.getN()); }
+	//	bool	operator>(const ExampleClass& ex) const { return (_ptr > ex.getPtr()); }
+};
+
+std::ostream&	operator<<(std::ostream& os, const ExampleClass& ex) { os << ex.getN() << " " << ex.getPtr(); return (os); }
+
 template<typename T>
 void	foo(T & param) {
 	param += 48;
+}
+
+template<>
+void	foo<ExampleClass>(ExampleClass& ex) {
+	ex.setN(ex.getN() + 48);
+	ex.setPtr((void*)((long)(ex.getPtr()) + 48));
 }
 
 template<typename T>
--- 07_cpp/ex01/src/main.cpp
+++ 07_cpp/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:40:20 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/18 15:51:59 by locharve         ###   ########.fr       */
+/*   Updated: 2025/01/02 08:52:43 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,48 @@ int	main() {
 		return (3);
 	}
 
+	ExampleClass*	exTab = new ExampleClass[10];
+	if (!exTab) {
+		delete [] intTab;
+		delete [] strTab;
+		delete [] fltTab;
+		return (4);
+	}
+
 	for (int i = 0; i < 10; i++) {
 		intTab[i] = i;
-		strTab[i] += i + 48;
+		strTab[i] = i + 48;
 		fltTab[i] = i;
+		exTab[i] = i;
 	}
 
 	std::cout << std::endl << "/////// Before iter:" << std::endl;
 	for (int i = 0; i < 10; i++) {
-		std::cout << "i = " << i << "\tint: " << intTab[i]
-			<< "\tstr: " << strTab[i] << "\tflt: " << fltTab[i] << std::endl;
+		std::cout << "i = " << i
+			<< "\tint: " << intTab[i]
+			<< "\tstr: " << strTab[i]
+			<< "\tflt: " << fltTab[i]
+			<< "\tex : " << exTab[i]
+			<< std::endl;
 	}
 
 	::iter(intTab, 10, foo);
 	::iter(strTab, 10, foo);
 	::iter(fltTab, 10, foo);
+	::iter(exTab, 10, foo);
 
 	std::cout << std::endl << "/////// After iter:" << std::endl;
 	for (int i = 0; i < 10; i++) {
-		std::cout << "i = " << i << "\tint: " << intTab[i]
-			<< "\tstr: " << strTab[i] << "\tflt: " << fltTab[i] << std::endl;
+		std::cout << "i = " << i
+		<< "\tint: " << intTab[i]
+		<< "\tstr: " << strTab[i]
+		<< "\tflt: " << fltTab[i]
+		<< "\tex : " << exTab[i]
+		<< std::endl;
 	}
 
 	delete [] intTab;
 	delete [] strTab;
 	delete [] fltTab;
+	delete [] exTab;
 }

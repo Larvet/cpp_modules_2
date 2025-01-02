47ad1a051d46b90d885bfe84efd3d8da24186cc0
Louis Charvet <locharve@2C4.42angouleme.fr>
2025-01-02 11:40:15 +0100

    ex00

--- 07_cpp/ex00/header/whatever.hpp
+++ 07_cpp/ex00/header/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 14:48:48 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/18 15:24:32 by locharve         ###   ########.fr       */
+/*   Updated: 2025/01/02 08:30:03 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,28 @@ template<typename T>
 T const&	max(T const& a, T const& b) {
 	return (a > b ? a : b);
 }
+
+class	ExampleClass {
+	private:
+		int	_n;
+		void*	_ptr;
+	public:
+		ExampleClass(int n, void* ptr): _n(n), _ptr(ptr) {}
+		ExampleClass() { _n = 42; _ptr = NULL; }
+		ExampleClass(const ExampleClass& src) { *this = src; }
+		ExampleClass&	operator=(const ExampleClass& src) { _n = src.getN(); _ptr = src.getPtr(); return (*this); }
+		~ExampleClass() {}
+
+		int	getN() const { return (_n); }
+		void*	getPtr() const { return (_ptr); }
+
+		bool	operator<(const ExampleClass& ex) const { return (_n < ex.getN()); }
+	//	bool	operator<(const ExampleClass& ex) const { return (_ptr < ex.getPtr()); }		
+
+		bool	operator>(const ExampleClass& ex) const { return (_n > ex.getN()); }
+	//	bool	operator>(const ExampleClass& ex) const { return (_ptr > ex.getPtr()); }
+};
+
+std::ostream&	operator<<(std::ostream& os, const ExampleClass& ex) { os << ex.getN() << " " << ex.getPtr(); return (os); }
 
 #endif
--- 07_cpp/ex00/src/main.cpp
+++ 07_cpp/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 14:50:49 by locharve          #+#    #+#             */
-/*   Updated: 2024/12/18 15:24:26 by locharve         ###   ########.fr       */
+/*   Updated: 2025/01/02 08:30:13 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,37 @@ int	main() {
 	char*	cs1 = strdup("oui oui");
 	char*	cs2 = strdup("non non");
 	std::string	s1("ouais gros"), s2("c'est fatal bazooka");
+	ExampleClass	ex1(b, &s1);
+	ExampleClass	ex2(726, &b);
 
 	std::cout << "/////// Before swap:" << std::endl
 		<< "a = " << a << "\tb = " << b << std::endl
 		<< "cs1 = " << cs1 << "\tcs2 = " << cs2 << std::endl
-		<< "s1 = " << s1 << "\ts2 = " << s2 << std::endl << std::endl;
+		<< "s1 = " << s1 << "\ts2 = " << s2 << std::endl
+		<< "ex1 = " << ex1 << "\tex2 = " << ex2 << std::endl << std::endl;
 
 	::swap<int>(a, b);
 	::swap<char*>(cs1, cs2);
 	::swap<std::string>(s1, s2);
+	::swap<ExampleClass>(ex1, ex2);
 
 	std::cout << "/////// After swap:" << std::endl
 		<< "a = " << a << "\tb = " << b << std::endl
 		<< "cs1 = " << cs1 << "\tcs2 = " << cs2 << std::endl
-		<< "s1 = " << s1 << "\ts2 = " << s2 << std::endl << std::endl;
+		<< "s1 = " << s1 << "\ts2 = " << s2 << std::endl
+		<< "ex1 = " << ex1 << "\tex2 = " << ex2 << std::endl << std::endl;
 
 	std::cout << "/////// min:" << std::endl
 		<< "min(a, b) = " << ::min<int>(a, b) << std::endl
 		<< "min(cs1, cs2) = " << ::min<char*>(cs1, cs2) << std::endl // doesn't work because char* has no overloaded operator
-		<< "min(s1, s2) = " << ::min<std::string>(s1, s2) << std::endl << std::endl;
+		<< "min(s1, s2) = " << ::min<std::string>(s1, s2) << std::endl
+		<< "min(ex1, ex2) = " << ::min<ExampleClass>(ex1, ex2) << std::endl << std::endl;
 
 	std::cout << "/////// max:" << std::endl
 		<< "max(a, b) = " << ::max<int>(a, b) << std::endl
 		<< "max(cs1, cs2) = " << ::max<char*>(cs1, cs2) << std::endl // doesn't work because char* has no overloaded operator
-		<< "max(s1, s2) = " << ::max<std::string>(s1, s2) << std::endl << std::endl;
+		<< "max(s1, s2) = " << ::max<std::string>(s1, s2) << std::endl
+		<< "max(ex1, ex2) = " << ::max<ExampleClass>(ex1, ex2) << std::endl << std::endl;
 
 	free (cs1);
 	free (cs2);
